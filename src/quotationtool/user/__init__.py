4f6d60ce2512f38d53c4ebe2ba51c45e42fc7ba2
Christian Lück <christian.lueck@ruhr-uni-bochum.de>
2011-10-12 17:51:07 +0200

    Permission for SignUp reworked
    SignUp now requires quotationtool.SignUp. It is granted to
    quotationtool.Visitor and quotationtool.Manager. So managers can add
    users in closed sites.
    
    A 'admin' principal is created by default! It should be removed or
    password should be changed!

--- src/quotationtool/user/__init__.py
+++ src/quotationtool/user/__init__.py
@@ -2,6 +2,8 @@ import zope.component
 from zope.authentication.interfaces import IAuthentication
 from zope.pluggableauth.authentication import PluggableAuthentication
 from zope.pluggableauth.plugins.session import SessionCredentialsPlugin
+from zope.security.proxy import removeSecurityProxy
+from zope.securitypolicy.interfaces import IPrincipalRoleManager
 
 from quotationtool.site.interfaces import INewQuotationtoolSiteEvent
 from signup import SignupPrincipalFolder
@@ -9,6 +11,8 @@ from signup import SignupPrincipalFolder
 
 @zope.component.adapter(INewQuotationtoolSiteEvent)
 def createPAU(event):
+    """ Create a pau (pluggable authentication utility, a signup
+    principal folder in it and a manger user (!)."""
     sm = event.object.getSiteManager()
 
     sm['default']['pau'] = pau = PluggableAuthentication()
@@ -18,6 +22,11 @@ def createPAU(event):
     pau.authenticatorPlugins = [folder.__name__]
     folder.prefix = u"users."
     folder.signup_roles = [u'quotationtool.Member']
+
+    principal_id = folder.signUp('admin', 'Op3n', 'Quotationtool Manager')
+    role_manager = IPrincipalRoleManager(event.object)
+    role_manager = removeSecurityProxy(role_manager)
+    role_manager.assignRoleToPrincipal('quotationtool.Manager', principal_id)
 
     pau['SessionCredentialsPlugin'] = session_creds = SessionCredentialsPlugin()
     pau.credentialsPlugins = [session_creds.__name__]
